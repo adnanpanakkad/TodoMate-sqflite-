291b9cba5da4b8834b64b867bc83915f9b3ceb3b
Adnan <adnancpanakkad@gmail.com>
2024-03-22 14:38:57 +0530

    creative mode

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DrawerNavigation extends StatefulWidget {
+  const DrawerNavigation({super.key});
+
+  @override
+  State<DrawerNavigation> createState() => _DrawerNavigationState();
+}
+
+class _DrawerNavigationState extends State<DrawerNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
